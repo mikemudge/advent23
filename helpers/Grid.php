e9fefa5f186c56375d21965a9fd5cbb613c884ca
Michael <mike.mudge@gmail.com>
2024-01-27 12:42:36 +1300

    Solve day 10

--- helpers/Grid.php
+++ helpers/Grid.php
@@ -2,11 +2,13 @@
 
 class GridLocation {
     private mixed $data;
-    private string $key;
+    private ?string $key;
     private int $x;
     private int $y;
+    private Grid $grid;
 
-    public function __construct(int $x, int $y, string $key) {
+    public function __construct(Grid $grid, int $x, int $y, ?string $key) {
+        $this->grid = $grid;
         $this->x = $x;
         $this->y = $y;
         $this->key = $key;
@@ -32,13 +34,43 @@ class GridLocation {
     public function __toString() {
         return "$this->x,$this->y ($this->key)";
     }
+
+    public function north(): ?GridLocation {
+        return $this->grid->get($this->x, $this->y - 1);
+    }
+
+    public function south(): ?GridLocation {
+        return $this->grid->get($this->x, $this->y + 1);
+    }
+
+    public function west(): ?GridLocation {
+        return $this->grid->get($this->x - 1, $this->y);
+    }
+
+    public function east(): ?GridLocation {
+        return $this->grid->get($this->x + 1, $this->y);
+    }
+
+    public function getDir(int $dir): ?GridLocation {
+        return match ($dir) {
+            0 => $this->north(),
+            1 => $this->east(),
+            2 => $this->south(),
+            3 => $this->west(),
+            default => throw new RuntimeException("Unknown dir $dir"),
+        };
+    }
+
+    public function setKey(string $key) {
+        $this->key = $key;
+    }
 }
 
 class Grid {
     private int $height;
     private int $width;
     private array $data;
-    private null $oobValue;
+    private GridLocation $oobValue;
 
     /**
      * @param string[] $lines
@@ -51,10 +83,10 @@ class Grid {
             $line = $lines[$y];
             $this->data[$y] = [];
             for ($x = 0; $x < $this->width; $x++) {
-                $this->data[$y][] = new GridLocation($x, $y, $line[$x]);
+                $this->data[$y][] = new GridLocation($this, $x, $y, $line[$x]);
             }
         }
-        $this->oobValue = null;
+        $this->oobValue = new GridLocation($this, PHP_INT_MIN, PHP_INT_MIN, null);
     }
 
     public function getWidth(): int {
@@ -92,7 +124,7 @@ class Grid {
         return $result;
     }
 
-    public function find(string $string) {
+    public function find(string $string): ?GridLocation {
         for ($y = 0; $y < $this->height; $y++) {
             for ($x = 0; $x < $this->width; $x++) {
                 if ($this->data[$y][$x]->getKey() == $string) {
--- puzzle10/solution.php
+++ puzzle10/solution.php
@@ -8,18 +8,168 @@ $part1 = 0;
 $part2 = 0;
 
 // sample
-$lines = [
-    "-L|F7",
-    "7S-7|",
-    "L|7||",
-    "-L-J|",
-    "L|-JF",
-];
+//$lines = [
+//    "-L|F7",
+//    "7S-7|",
+//    "L|7||",
+//    "-L-J|",
+//    "L|-JF",
+//];
+//// sample
+//$lines = [
+//    "7-F7-",
+//    ".FJ|7",
+//    "SJLL7",
+//    "|F--J",
+//    "LJ.LJ",
+//];
 
 $grid = new Grid($lines);
 
 $loc = $grid->find("S");
 
-echo ("$loc\n");
+$u = $loc->north()->getKey();
+$d = $loc->south()->getKey();
+$l = $loc->west()->getKey();
+$r = $loc->east()->getKey();
+$north = false;
+$south = false;
+$east = false;
+$west = false;
+if ($u == "|" || $u == "F" || $u == "7") {
+    $north = true;
+}
+if ($d == "|" || $d == "L" || $d == "J") {
+    $south = true;
+}
+if ($l == "-" || $l == "L" || $l == "F") {
+    $west = true;
+}
+if ($r == "-" || $r == "7" || $r == "J") {
+    $east = true;
+}
+
+echo("$u,$d,$l,$r => $north,$south,$west,$east\n");
+// NESW = 0123
+$dir = 0;
+if ($north) {
+    if ($east) {
+        $cur = "L";
+    } else if ($west) {
+        $cur = "J";
+    } else if ($south) {
+        $cur = "|";
+    }
+} else if ($south) {
+    $dir = 2;
+    if ($east) {
+        $cur = "F";
+    } else if ($west) {
+        $cur = "7";
+    }
+} else {
+    $dir = 1;
+    $cur = "-";
+}
+
+$loc->setKey($cur);
+echo("Dir: $dir, Cur: $cur\n");
+function updateDir(int $dir, string $pipe) {
+    switch ($dir) {
+        case 0:
+            return match ($pipe) {
+                "|" => 0,
+                "7" => 3,
+                "F" => 1,
+                default => throw new RuntimeException("Unknown pipe for north $pipe"),
+            };
+        case 1:
+            return match ($pipe) {
+                "-" => 1,
+                "7" => 2,
+                "J" => 0,
+                default => throw new RuntimeException("Unknown pipe for east $pipe"),
+            };
+        case 2:
+            return match ($pipe) {
+                "|" => 2,
+                "L" => 1,
+                "J" => 3,
+                default => throw new RuntimeException("Unknown pipe for south $pipe"),
+            };
+        case 3:
+            return match ($pipe) {
+                "-" => 3,
+                "L" => 0,
+                "F" => 2,
+                default => throw new RuntimeException("Unknown pipe for west $pipe"),
+            };
+    }
+}
+
+// Follow $dir and build a path.
+$iter = $loc->getDir($dir);
+$iter->setData(true);
+$dir = updateDir($dir, $iter->getKey());
+$path = [$iter];
+while($iter !== $loc) {
+    $iter = $iter->getDir($dir);
+    $iter->setData(true);
+    $path[] = $iter;
+    if ($iter->getKey() == "S") {
+        break;
+    }
+    $dir = updateDir($dir, $iter->getKey());
+}
+
+$pathLength = count($path);
+echo("Length: $pathLength\n");
+$part1 = $pathLength / 2;
+
 echo("Part 1: $part1" . PHP_EOL);
+
+for ($y = 0; $y < $grid->getHeight(); $y++) {
+    $insideLoop = false;
+    $crossedLoop = false;
+    $entry = null;
+    for ($x = 0; $x < $grid->getWidth(); $x++) {
+        $loc = $grid->get($x, $y);
+        if ($loc->getData() === true) {
+            echo($loc->getKey());
+            if (!$crossedLoop) {
+                $entry = $loc->getKey();
+                if ($entry == "|") {
+                    // Simple crossing.
+                    $insideLoop = !$insideLoop;
+                } else {
+                    // Start a crossing.
+                    $crossedLoop = true;
+                }
+            } else {
+                // A continued crossing.
+                if ($loc->getKey() != "-") {
+                    // The crossing ends with a non -
+                    $exit = $loc->getKey();
+                    if ($entry == "F" && $exit == "J") {
+                        $insideLoop = !$insideLoop;
+                    } else if ($entry == "L" && $exit == "7") {
+                        $insideLoop = !$insideLoop;
+                    }
+                    // No longer crossing.
+                    $crossedLoop = false;
+                }
+            }
+        } else {
+            // This tile is not part of the loop
+            if ($insideLoop) {
+                $part2++;
+                echo("I");
+            } else {
+                echo("O");
+            }
+        }
+    }
+    echo("\n");
+}
+
 echo("Part 2: $part2" . PHP_EOL);
